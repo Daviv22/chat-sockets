a842ad3b05a0e225544de5d1d212271e21388122
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-06-07 01:24:21 -0300

    Estrutura básica para componente Contacts

---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,10 @@
+export default function Contacts() {
+    return (
+        <div className="">
+            <h2>Seus Contatos</h2>
+            <div>
+                {/*Lista de grupos aqui*/}
+            </div>
+        </div>
+    )
+}
